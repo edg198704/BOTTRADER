f851709538375dcab8c4865777057240fc7ba22d
edg198704 <edg198704@gmail.com>
2025-11-17 20:33:15 -0400

    Delete bot_ai_enterprise_refactored.py

--- bot_ai_enterprise_refactored.py
+++
@@ -1,7 +0,0 @@
-# DEPRECATED
-# The logic from this file has been migrated to the modular `bot_core` architecture.
-# All core components like ExchangeManager, PositionManager, RiskManager, and the main bot
-# orchestration logic are now located in their respective modules inside the `bot_core` package.
-# This file is now obsolete and should not be used.
-
-pass
