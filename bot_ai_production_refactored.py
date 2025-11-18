48dbd23f7f6db493e14486001c797f4cbf359794
edg198704 <edg198704@gmail.com>
2025-11-17 20:33:42 -0400

    Delete bot_ai_production_refactored.py

--- bot_ai_production_refactored.py
+++
@@ -1,8 +0,0 @@
-# DEPRECATED
-# The logic from this file has been migrated to the modular `bot_core` architecture.
-# - TelegramKillSwitch -> `bot_core/telegram_bot.py`
-# - PositionLedger -> `bot_core/position_manager.py`
-# - Other components have also been moved to their respective modules.
-# This file is now obsolete and should not be used.
-
-pass
