fcde2df4d23aa422256bbff36cfff3cbc364ba23
edg198704 <edg198704@gmail.com>
2025-11-17 20:33:34 -0400

    Delete bot_ai_production_complete.py

--- bot_ai_production_complete.py
+++
@@ -1,5 +0,0 @@
-# DEPRECATED
-# The logic from this file has been fully migrated to the modular `bot_core` architecture.
-# This file is now obsolete and should not be used. The main entry point for the bot is `start_bot.py`.
-
-pass
