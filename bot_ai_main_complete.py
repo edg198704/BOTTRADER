c7ab6de73f15956640894623896e5361169e5b49
edg198704 <edg198704@gmail.com>
2025-11-17 20:33:26 -0400

    Delete bot_ai_main_complete.py

--- bot_ai_main_complete.py
+++
@@ -1,7 +0,0 @@
-# DEPRECATED
-# The logic from this file has been migrated to the modular `bot_core` architecture.
-# The main bot class is now `TradingBot` in `bot_core/bot.py`.
-# The main execution entry point is `start_bot.py`.
-# This file is now obsolete and should not be used.
-
-pass
